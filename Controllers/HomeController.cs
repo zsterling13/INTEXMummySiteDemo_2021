a26906740e3c4bacb124acaec1c6307627bd8403
brykek <64246135+brykek@users.noreply.github.com>
2021-04-12 23:46:46 -0600

    edited controller for edit action

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -182,104 +182,105 @@ namespace INTEX2Mock.Controllers
                     x.FieldBook = passedMummy.FieldBook;
                     x.FieldBookPageNum = passedMummy.FieldBookPageNum;
                     x.InitialOfDataExpert = passedMummy.InitialOfDataExpert;
-                    //    public string InitialsChecker { get; set; }
-                    //    public string BodyAnalyzeYear { get; set; }
-                    //    public string SouthToHead { get; set; }
-                    //    public string SouthToFeet { get; set; }
-                    //    public string EastToHead { get; set; }
-                    //    public string EastToFeet { get; set; }
-                    //    public string WestToHead { get; set; }
-                    //    public string WestToFeet { get; set; }
-                    //    public string BurialSituation { get; set; }
-                    //    public string LengthOfRemains { get; set; }
-                    //    public int? SampleNumber { get; set; }
-                    //    public string GeFunctionTotal { get; set; }
-                    //    public string GenderBodyCol { get; set; }
-                    //    public string HowGenderDetermined { get; set; }
-                    //    public string BasilarSuture { get; set; }
-                    //    public string VentralArc { get; set; }
-                    //    public string SubpubicAngle { get; set; }
-                    //    public string SciaticNotch { get; set; }
-                    //    public string PubicBone { get; set; }
-                    //    public string PreaurSulcus { get; set; }
-                    //    public string MedialIpRamus { get; set; }
-                    //    public string DorsalPitting { get; set; }
-                    //    public string ForemanMagnum { get; set; }
-                    //    public string FemurHead { get; set; }
-                    //    public string HumerusHead { get; set; }
-                    //    public string Osteophytosis { get; set; }
-                    //    public string PubicSymphysis { get; set; }
-                    //    public string BoneLength { get; set; }
-                    //    public string MedialClavicle { get; set; }
-                    //    public string IliacCrest { get; set; }
-                    //    public string FemurDiameter { get; set; }
-                    //    public string Humerus { get; set; }
-                    //    public string FemurLength { get; set; }
-                    //    public string HumerusLength { get; set; }
-                    //    public string TibiaLength { get; set; }
-                    //    public string Robust { get; set; }
-                    //    public string SupraorbitalRidges { get; set; }
-                    //    public string OrbitEdge { get; set; }
-                    //    public string ParietalBossing { get; set; }
-                    //    public string Gonian { get; set; }
-                    //    public string NuchalCrest { get; set; }
-                    //    public string ZygomaticCrest { get; set; }
-                    //    public string CranialSuture { get; set; }
-                    //    public string MaximumCranialLength { get; set; }
-                    //    public string MaximumCranialBreadth { get; set; }
-                    //    public string BasionBregmaHeight { get; set; }
-                    //    public string BasionNasion { get; set; }
-                    //    public string BasionProsthionLength { get; set; }
-                    //    public string BizygomaticDiameter { get; set; }
-                    //    public string NasionProsthion { get; set; }
-                    //    public string MaximumNasalBreadth { get; set; }
-                    //    public string InterorbitalBreadth { get; set; }
-                    //    public string ArtifactsDescription { get; set; }
-                    //    public string HairColor { get; set; }
-                    //    public string PreservationIndex { get; set; }
-                    //    public bool? HairTaken { get; set; }
-                    //    public bool? SoftTissueTaken { get; set; }
-                    //    public bool? BoneTaken { get; set; }
-                    //    public bool? ToothTaken { get; set; }
-                    //    public bool? TextileTaken { get; set; }
-                    //    public string DescriptionOfTaken { get; set; }
-                    //    public bool? ArtifactFound { get; set; }
-                    //    public string AgeMethod { get; set; }
-                    //    public string EstimateAge { get; set; }
-                    //    public string AgeClassification { get; set; }
-                    //    public string EstimateLivingStature { get; set; }
-                    //    public string ToothAttrition { get; set; }
-                    //    public string ToothEruption { get; set; }
-                    //    public string PathologyAnomalies { get; set; }
-                    //    public string EpiphysealUnion { get; set; }
-                    //    public string YearFound { get; set; }
-                    //    public string MonthFound { get; set; }
-                    //    public string DayFound { get; set; }
-                    //    public string HeadDirection { get; set; }
-                    //    public string Rack { get; set; }
-                    //    public string CribraOrbitala { get; set; }
-                    //    public string PoroticHyperostosis { get; set; }
-                    //    public string PoroticHyperostosisLocations { get; set; }
-                    //    public string MetopicSuture { get; set; }
-                    //    public string ButtonOsteoma { get; set; }
-                    //    public string PostcraniaTrauma { get; set; }
-                    //    public string OsteologyUnknownComment { get; set; }
-                    //    public string TemporalMandibularJointOsteoarthritisTmjOa { get; set; }
-                    //    public string LinearHypoplasiaEnamel { get; set; }
-                    //    public string AreaHillBurials { get; set; }
-                    //    public string Tomb { get; set; }
-                    //    public string YearOnSkull { get; set; }
-                    //    public string MonthOnSkull { get; set; }
-                    //    public string DateOnSkull { get; set; }
-                    //    public string Cluster { get; set; }
-                    //    public string Notes { get; set; }
-                    //    public string Shaft { get; set; }
-                    //    public string SharedShaft { get; set; }
-                    //    public string PreservationIndex2 { get; set; }
-                    //    public string BurialMaterials { get; set; }
-                    //    public string BurialGoods { get; set; }
-                    //    public string Photo { get; set; }
-                    }
+                    x.InitialsChecker = passedMummy.InitialsChecker;
+                    x.BodyAnalyzeYear = passedMummy.BodyAnalyzeYear;
+                    x.SouthToHead = passedMummy.SouthToHead;
+                    x.SouthToFeet = passedMummy.SouthToFeet;
+                    x.EastToHead = passedMummy.EastToHead;
+                    x.EastToFeet = passedMummy.EastToFeet;
+                    x.WestToHead = passedMummy.WestToHead;
+                    x.WestToFeet = passedMummy.WestToFeet;
+                    x.BurialSituation = passedMummy.BurialSituation;
+                    x.LengthOfRemains = passedMummy.LengthOfRemains;
+                    x.SampleNumber = passedMummy.SampleNumber;
+                    x.GeFunctionTotal = passedMummy.GeFunctionTotal;
+                    x.GenderBodyCol = passedMummy.GenderBodyCol;
+                    x.HowGenderDetermined = passedMummy.HowGenderDetermined;
+                    x.BasilarSuture = passedMummy.BasilarSuture;
+                    x.VentralArc = passedMummy.VentralArc;
+                    x.SubpubicAngle = passedMummy.SubpubicAngle;
+                    x.SciaticNotch = passedMummy.SciaticNotch;
+                    x.PubicBone = passedMummy.PubicBone;
+                    x.PreaurSulcus = passedMummy.PreaurSulcus;
+                    x.MedialIpRamus = passedMummy.MedialIpRamus;
+                    x.DorsalPitting = passedMummy.DorsalPitting;
+                    x.ForemanMagnum = passedMummy.ForemanMagnum;
+                    x.FemurHead = passedMummy.FemurHead;
+                    x.HumerusHead = passedMummy.HumerusHead;
+                    x.Osteophytosis = passedMummy.Osteophytosis;
+                    x.PubicSymphysis = passedMummy.PubicSymphysis;
+                    x.BoneLength = passedMummy.BoneLength;
+                    x.MedialClavicle = passedMummy.MedialClavicle;
+                    x.IliacCrest = passedMummy.IliacCrest;
+                    x.FemurDiameter = passedMummy.FemurDiameter;
+                    x.Humerus = passedMummy.Humerus;
+                    x.FemurLength = passedMummy.FemurLength;
+                    x.HumerusLength = passedMummy.HumerusLength;
+                    x.TibiaLength = passedMummy.TibiaLength;
+                    x.Robust = passedMummy.Robust;
+                    x.SupraorbitalRidges = passedMummy.SupraorbitalRidges;
+                    x.OrbitEdge = passedMummy.OrbitEdge;
+                    x.ParietalBossing = passedMummy.ParietalBossing;
+                    x.Gonian = passedMummy.Gonian;
+                    x.NuchalCrest = passedMummy.NuchalCrest;
+                    x.ZygomaticCrest = passedMummy.ZygomaticCrest;
+                    x.CranialSuture = passedMummy.CranialSuture;
+                    x.MaximumCranialLength = passedMummy.MaximumCranialLength;
+                    x.MaximumCranialBreadth = passedMummy.MaximumCranialBreadth;
+                    x.BasionBregmaHeight = passedMummy.BasionBregmaHeight;
+                    x.BasionNasion = passedMummy.BasionNasion;
+                    x.BasionProsthionLength = passedMummy.BasionProsthionLength;
+                    x.BizygomaticDiameter = passedMummy.BizygomaticDiameter;
+                    x.NasionProsthion = passedMummy.NasionProsthion;
+                    x.MaximumNasalBreadth = passedMummy.MaximumNasalBreadth;
+                    x.InterorbitalBreadth = passedMummy.InterorbitalBreadth;
+                    x.ArtifactsDescription = passedMummy.ArtifactsDescription;
+                    x.HairColor = passedMummy.HairColor;
+                    x.PreservationIndex = passedMummy.PreservationIndex;
+                    x.HairTaken = passedMummy.HairTaken;
+                    x.SoftTissueTaken = passedMummy.SoftTissueTaken;
+                    x.BoneTaken = passedMummy.BoneTaken;
+                    x.ToothTaken = passedMummy.ToothTaken;
+                    x.TextileTaken = passedMummy.TextileTaken;
+                    x.DescriptionOfTaken = passedMummy.DescriptionOfTaken;
+                    x.ArtifactFound = passedMummy.ArtifactFound;
+                    x.AgeMethod = passedMummy.AgeMethod;
+                    x.EstimateAge = passedMummy.EstimateAge;
+                    x.AgeClassification = passedMummy.AgeClassification;
+                    x.EstimateLivingStature = passedMummy.EstimateLivingStature;
+                    x.ToothAttrition = passedMummy.ToothAttrition;
+                    x.ToothEruption = passedMummy.ToothEruption;
+                    x.PathologyAnomalies = passedMummy.PathologyAnomalies;
+                    x.EpiphysealUnion = passedMummy.EpiphysealUnion;
+                    x.YearFound = passedMummy.YearFound;
+                    x.MonthFound = passedMummy.MonthFound;
+                    x.DayFound = passedMummy.DayFound;
+                    x.HeadDirection = passedMummy.HeadDirection;
+                    x.Rack = passedMummy.Rack;
+                    x.CribraOrbitala = passedMummy.CribraOrbitala;
+                    x.PoroticHyperostosis = passedMummy.PoroticHyperostosis;
+                    x.PoroticHyperostosisLocations = passedMummy.PoroticHyperostosisLocations;
+                    x.MetopicSuture = passedMummy.MetopicSuture;
+                    x.ButtonOsteoma = passedMummy.ButtonOsteoma;
+                    x.PostcraniaTrauma = passedMummy.PostcraniaTrauma;
+                    x.OsteologyUnknownComment = passedMummy.OsteologyUnknownComment;
+                    x.TemporalMandibularJointOsteoarthritisTmjOa = passedMummy.TemporalMandibularJointOsteoarthritisTmjOa;
+                    x.LinearHypoplasiaEnamel = passedMummy.LinearHypoplasiaEnamel;
+                    x.LinearHypoplasiaEnamel = passedMummy.LinearHypoplasiaEnamel;
+                    x.AreaHillBurials = passedMummy.AreaHillBurials;
+                    x.Tomb = passedMummy.Tomb;
+                    x.YearOnSkull = passedMummy.YearOnSkull;
+                    x.MonthOnSkull = passedMummy.MonthOnSkull;
+                    x.DateOnSkull = passedMummy.DateOnSkull;
+                    x.Cluster = passedMummy.Cluster;
+                    x.Notes = passedMummy.Notes;
+                    x.Shaft = passedMummy.Shaft;
+                    x.SharedShaft = passedMummy.SharedShaft;
+                    x.PreservationIndex2 = passedMummy.PreservationIndex2;
+                    x.BurialMaterials = passedMummy.BurialMaterials;
+                    x.BurialGoods = passedMummy.BurialGoods;
+                    x.Photo = passedMummy.Photo;
+                }
 
                     _mummyContext.SaveChanges();
 
